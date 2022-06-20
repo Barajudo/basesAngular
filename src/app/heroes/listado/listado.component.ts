a9d0495f43d279848f152cd921da5eaee3791e0c
Barajudo <daniel.barajas.lopez.93@gmail.com>
2022-06-20 12:32:55 -0500

    Primer commit, se finaliza la seccion 4 del curso

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+
+  heroes: string[] = [
+    'Spiderman', 
+    'Dr. Strange', 
+    'Thor', 
+    'Hulk',
+    'Wanda'
+  ];
+
+  heroeBorrado: string = '';
+
+  borrarHeroe(): void {
+    this.heroeBorrado = this.heroes.pop() || '';
+  }
+
+}
